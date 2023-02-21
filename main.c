3f27142b1de04bec374598ffd540c0d59ff699cf
AJZ30 <113471724+AJZ30@users.noreply.github.com>
2023-02-21 19:22:40 +0000

    test

--- main.c
+++ main.c
@@ -1,59 +1,36 @@
 #include <stdio.h>
 
-#include <stdio.h>
-
-
-
 int main (void){
-
     int choice = 0;
-
     char name = 0;
 
 // Starting the Program
-
     printf("Stock Market Tracker & Portfolio\n");
-
     printf("What is your name?\n");
-
     scanf("%s", &name);
 
 // Prompting the user input
 
-
-
     printf("What would you like to do?\n1. View Portfolio\n2. Purchase Stocks\n3. Sell Stocks\n4. View Market Prices\n");
-
     scanf("%d", &choice);
-
     if (choice == 1){
-
         puts("Your Portfolio");
-
     }
 
     else if (choice == 2) {
-
         puts("Buy Stock");
-
     }
 
     else if (choice == 3) {
-
         puts("Sell Stocks");
-
     }
 
     else if (choice == 4) {
-
         puts ("View Prices");
-
     }
 
     else {
-
         puts("Not a valid response.");
-
     }
 
     return (0);
